1f042659be00d4954715c156c2c426442076618c
Alexandr Kogut <dev.alexandr.kogut@gmail.com>
2022-05-24 19:22:50 +0300

    [HW2] Create Task3

---
+++ src/Lesson2/Task3.java
@@ -0,0 +1,13 @@
+package Lesson2;
+
+public class Task3 {
+    public static void main(String[] args) {
+        String string1 = "\nмоя строка 1";
+        String string2 = "\tмоя строка 2";
+//        String string3 = "\aмоя строка 3";
+
+        System.out.println(string1);
+        System.out.println(string2);
+//        System.out.println(string3);
+    }
+}

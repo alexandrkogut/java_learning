48ba0802b803f4c4d189b397a46dd6a4a84c480b
Alexandr Kogut <dev.alexandr.kogut@gmail.com>
2022-05-31 23:35:36 +0300

    [HW3] Create Task4

---
+++ src/Lesson3/Volume.java
@@ -0,0 +1,21 @@
+package Lesson3;
+
+import java.util.Scanner;
+
+public class Volume {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Input a radius: ");
+        int radius = in.nextInt();
+
+        System.out.print("Input a height: ");
+        int height = in.nextInt();
+
+        double v = Circle.PI * Math.pow(radius,2) * height;
+        System.out.println("V is " + v);
+
+        double s = 2 * Circle.PI * radius * (height + radius);
+        System.out.println("S is " + s);
+    }
+}
